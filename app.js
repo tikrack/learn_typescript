05b9ff154c43cf7e339659582432c90e08afbc22
tikrack <tikrackcode@gmail.com>
2025-10-21 16:16:13 +0330

    [Enums]

--- app.js
+++ app.js
@@ -5,16 +5,8 @@ let str = "Hello World!";
 let int = 1;
 let bool = false;
 // -------------------------Array
-let arr = [
-    "TEst",
-    "TEST",
-    "test"
-];
-let arr2 = [
-    "Heloo",
-    10,
-    true
-];
+let arr = ["TEst", "TEST", "test"];
+let arr2 = ["Heloo", 10, true];
 // -------------------------Tuple
 let tuple = [10, "20"];
 // -------------------------Specially
@@ -25,4 +17,18 @@ let username = null;
 username = "Hello";
 let password = false;
 password = 123456;
+// -------------------------Void
+//the voids function not return any things!
+function log(message) {
+    console.log(message);
+}
+// -------------------------Enum
+var Direction;
+(function (Direction) {
+    Direction[Direction["Up"] = 1] = "Up";
+    Direction[Direction["Down"] = 4] = "Down";
+    Direction[Direction["Left"] = 3] = "Left";
+    Direction[Direction["Right"] = 4] = "Right";
+})(Direction || (Direction = {}));
+let dir = Direction.Up;
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,43 +1,45 @@
 // -------------------------Basic
 
-let str :string = "Hello World!"
-let int :number = 1
-let bool :boolean = false
+let str: string = "Hello World!"
+let int: number = 1
+let bool: boolean = false
 
 // -------------------------Array
 
-let arr :string[] = [
-    "TEst",
-    "TEST",
-    "test"
-]
+let arr: string[] = ["TEst", "TEST", "test"]
 
-let arr2 :any[] = [
-    "Heloo",
-    10,
-    true
-]
+let arr2: any[] = ["Heloo", 10, true]
 
 // -------------------------Tuple
 
-let tuple :[number, string] = [10, "20"]
+let tuple: [number, string] = [10, "20"]
 
 // -------------------------Specially
 
-let nil :null = null
-let undef :undefined = undefined
+let nil: null = null
+let undef: undefined = undefined
 
 // -------------------------Union
 
-let username :string | null = null
+let username: string | null = null
 username = "Hello"
 
-let password :number | boolean = false
+let password: number | boolean = false
 password = 123456
 
 // -------------------------Void
 
 //the voids function not return any things!
-function log(message :string) :void {
+function log(message: string): void {
     console.log(message)
 }
+
+// -------------------------Enum
+enum Direction {
+    Up = 1,
+    Down = 4,
+    Left = 3,
+    Right
+}
+
+let dir: Direction = Direction.Up

38a1abcfd66f0c40172dec0fcbcc7601baba4568
tikrack <tikrackcode@gmail.com>
2025-10-21 21:06:00 +0330

    [Capsulation]

--- classes.ts
+++ classes.ts
@@ -1,6 +1,25 @@
-class Car {
-    public name: string = "kopk"
+class Car1 {
+    public model: string;
+
+    constructor(model: string) {
+        this.model = model;
+    }
+
+    getModel() {
+        return this.model;
+    }
 }
 
-const sh = new Car()
-console.log(sh.name)
+class Car2 {
+    constructor(public model: string) {}
+
+    getModel() {
+        return this.model;
+    }
+}
+
+let car1 = new Car1("401");
+let car2 = new Car2("206");
+
+console.log(car1.getModel());
+console.log(car2.getModel());

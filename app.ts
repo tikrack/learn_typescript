6a8fdb892ac663af03774be3fb277d6198ce4710
tikrack <tikrackcode@gmail.com>
2025-10-21 16:02:27 +0330

    [Array]: learn array and tuple

--- app.ts
+++ app.ts
@@ -1,3 +1,20 @@
 let str :string = "Hello World!"
 let int :number = 1
 let bool :boolean = false
+
+// -------------------------Array
+
+let arr :string[] = [
+    "TEst",
+    "TEST",
+    "test"
+]
+
+let arr2 :any[] = [
+    "Heloo",
+    10,
+    true
+]
+
+// -------------------------Tupple
+let tuple :[number, string] = [10, "20"]
